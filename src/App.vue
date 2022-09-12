2dc156e5f90d97b845518664d7f920ee4d7cb372
KENI0 <chl0933@naver.com>
2022-09-12 23:33:09 +0900

    렌더링 연습

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <Fruits />
+  <ul>
+<li v-for="{id, name} in 과일객체" :key="id">
+  {{ name }}
+</li>
+</ul>
 </template>
 
 <script>
-import Fruits from "~/components/Fruits";
-export default {
-  components: {
-    Fruits,
-  },
-};
+  import shortid from 'shortid'
+  export default{
+    data() {
+      return{
+      과일들 : ['딸기','포도','사과']
+      }
+    },
+    computed: {
+      과일객체() {
+        return this.과일들.map(과일 => {
+          return{
+            id: shortid.generate(),
+            name: 과일
+          }
+        })
+      }
+    }
+  }
+
 </script>
