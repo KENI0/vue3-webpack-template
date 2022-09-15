c85c80f8b06516f4f9d6fd5a6b26227240a753c2
KENI0 <chl0933@naver.com>
2022-09-15 18:32:40 +0900

    양방향 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
+<!-- 양방향 데이터 바인딩 -->
 <template>
-  <ul>
-<li v-for="{id, name} in 과일객체" :key="id">
-  {{ name }}
-</li>
-</ul>
+  <h1> {{msg}}</h1>
+<input type="text"
+  @input="msg = $event.target.value"
+>
+<input type="checkbox"
+    v-model="체크"
+>
+{{ 체크}}
+
 </template>
 
 <script>
-  import shortid from 'shortid'
-  export default{
+  export default {
     data() {
-      return{
-      과일들 : ['딸기','포도','사과']
+      return {
+        msg: "안녕!",
+        체크: false
+        
       }
     },
-    computed: {
-      과일객체() {
-        return this.과일들.map(과일 => {
-          return{
-            id: shortid.generate(),
-            name: 과일
-          }
-        })
-      }
     }
-  }
-
+  
 </script>
