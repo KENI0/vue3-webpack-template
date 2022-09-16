78628e101593f96036dd2392d00cdd49ba26c2d4
KENI0 <chl0933@naver.com>
2022-09-16 23:16:09 +0900

    component practice

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
-<!-- 양방향 데이터 바인딩 -->
 <template>
-  <h1> {{msg}}</h1>
-<input type="text"
-  @input="msg = $event.target.value"
->
-<input type="checkbox"
-    v-model="체크"
->
-{{ 체크}}
-
+  <Button>
+    <template v-slot:text>
+    <span>우영</span>
+    </template>
+    <template v-slot:icon>
+    <span>WOO</span>
+    </template>
+  </Button>
 </template>
 
-<script>
+<script> 
+import Button from '~/components/Mybutton'
   export default {
-    data() {
-      return {
-        msg: "안녕!",
-        체크: false
-        
-      }
-    },
+    components: {
+      Button
     }
-  
+  }
 </script>
---
+++ src/components/Mybutton.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="btn"
+>
+  <slot name="icon"></slot>
+  <slot name="text"></slot>
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+  .btn {
+    display: inline-block;
+    padding: 15px;
+    margin: 5px;
+    border-radius: 10px;
+    background-color: orange;
+  }
+
+</style>
