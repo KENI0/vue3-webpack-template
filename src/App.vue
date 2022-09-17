7a552f456c3fd7481b538757f3d167ef3f2e2b45
KENI0 <chl0933@naver.com>
2022-09-18 01:14:45 +0900

    emmit practice

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,24 @@
 <template>
-  <button @click="message = 'Good enough!'">
-    click here!
-  </button>
-
-  <h1> app: {{message}}</h1>
-  <Parent></Parent>
-
+  <hello @woo="클릭"
+        @우영="출력"
+  ></hello>
 </template>
 
 <script>
-import Parent from "~/components/parent";
-import { computed } from 'vue'
-export default {
-  components: {
-    Parent,
-  },
-  data() {
-    return {
-      message: "hello!",
-      글자색상: "orange",
-    };
-  },
-  provide(){
-    return{
-      msg: computed(() => {
-        return this.message 
-      }),
-    글자색: this.글자색상  
-    }
+  import hello from '~/components/hello'
+  export default {
+    components: {
+      hello 
+    },
+    methods: {
+      클릭: function(event) {
+        console.log('안녕!!')
+        console.log(event)
+      },
+      출력(msg) {
+        console.log(msg)
+      }
+
   }
-};
+}
 </script>
---
+++ src/components/hello.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>click here!</h1>
+<h2 @click="$emit('woo',$event)"> 안녕 ! </h2>
+<h3>{{msg }} </h3>
+<input type="text"
+      v-model="msg"
+      >
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg: ''
+    }
+  },
+  emits: [
+  'woo',
+  '우영'
+  ],
+  watch: {
+    msg() {
+      this.$emit('우영',this.msg)
+    }
+  }
+}
+</script>
