49f87844dfc73588ba2734e10c3fa8df7489eb21
KENI0 <chl0933@naver.com>
2022-09-17 21:42:45 +0900

    provide & inject example

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <Button>
-    <template v-slot:text>
-    <span>우영</span>
-    </template>
-    <template v-slot:icon>
-    <span>WOO</span>
-    </template>
-  </Button>
+  <button @click="message = 'Good enough!'">
+    click here!
+  </button>
+
+  <h1> app: {{message}}</h1>
+  <Parent></Parent>
+
 </template>
 
-<script> 
-import Button from '~/components/Mybutton'
-  export default {
-    components: {
-      Button
+<script>
+import Parent from "~/components/parent";
+import { computed } from 'vue'
+export default {
+  components: {
+    Parent,
+  },
+  data() {
+    return {
+      message: "hello!",
+      글자색상: "orange",
+    };
+  },
+  provide(){
+    return{
+      msg: computed(() => {
+        return this.message 
+      }),
+    글자색: this.글자색상  
     }
   }
+};
 </script>
---
+++ src/components/parent.vue
@@ -0,0 +1,12 @@
+<template>
+  <Child></Child>
+</template>
+
+<script>
+import Child from "~/components/child";
+export default {
+  components: {
+    Child,
+  },
+};
+</script>
