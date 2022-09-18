359baf0e658b0edf4f9119139ea1254c5cd38366
KENI0 <chl0933@naver.com>
2022-09-19 01:36:51 +0900

    composition api 연습

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
 <template>
-  <hello @woo="클릭"
-        @우영="출력"
-  ></hello>
+  <h1 @click="increase">
+    {{ count }} / {{ doubleCount}}
+  </h1>
+  <h1 @click="changeMessage">
+    {{ message }} / {{reversedMessage}}
+  </h1>
 </template>
 
 <script>
-  import hello from '~/components/hello'
-  export default {
-    components: {
-      hello 
-    },
-    methods: {
-      클릭: function(event) {
-        console.log('안녕!!')
-        console.log(event)
-      },
-      출력(msg) {
-        console.log(msg)
-      }
+import { ref, computed,watch } from "vue";
+export default {
+  setup() {
+    let count = ref(0);
+    const doubleCount = computed(() => {
+      return count.value * 2
+    })
+    function increase() {
+      count.value += 1;
+    }
+    let message = ref("helllo world");
+    const reversedMessage = computed(() => {
+      return message.value.split('').reverse().join('')
+    })
 
-  }
-}
+    watch(message,(newValue)=>{
+      console.log(newValue)
+    })
+
+    function changeMessage() {
+      message.value = "Good?!";
+    }
+
+    return {
+      count,
+      doubleCount,
+      message,
+      reversedMessage,
+      increase,
+      changeMessage,
+    };
+  },
+};
 </script>
